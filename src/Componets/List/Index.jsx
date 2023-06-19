1bab4f45fb3e99c0c45a1bf2a36f8f83eea05d1c
talafarael <127351345+talafarael@users.noreply.github.com>
2023-06-19 23:39:28 +0300

    Update Index.jsx

--- src/Componets/List/Index.jsx
+++ src/Componets/List/Index.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import Input from '../Input/Index'
 import useStore from '../../UseStore/Index'
+import './div.css'
 function ListItem(){
-  const listItem = useStore(state => state.listItem);
-
+   const closeItem = useStore((state) => state.closeItem);
+  const listItem = useStore(state =>state.listItem);
+ 
   console.log(listItem)
-
+  function closeWindow(h){
+    closeItem(closeItem(h) )
+  }
   return (
-   <ul>
-     
-   </ul>
+   <div className='main'>
+     {listItem.map((item)=>
+     <div className='box' key={item.id}>
+      <h1>{item.id }</h1>
+      <p>{item.notice}</p>
+      <button key={item.id} onClick={()=>closeWindow(item.id)}>close</button>
+     </div>)}
+   </div>
   )}
 
 export default ListItem
